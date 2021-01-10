1bd5db8ad96a3f177f46b8f6833996ad5eec7795
Kevin <admin@5trubel.de>
2021-01-10 03:07:16 +0100

    modified:   billjihad.bat 	new file:   billjihad.jpg

--- billjihad.bat
+++ billjihad.bat
@@ -1,3 +1,3 @@
 Z:\GarrysMod\gmad.exe billjihad
-Z:\GarrysMod\gmpublish.exe update -id 1962795988 -addon billjihad.gma -icon billjihad.jpg
+Z:\GarrysMod\gmpublish.exe update -id 1962795988 -addon billjihad.gma -icon billjihad.jpg -changes "See https://gitlab.com/DBotThePony/DBotProjects"
 rm touchme.gma
