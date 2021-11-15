c01fb45a2b1afdba19e3badec4ac95f897ea81e1
Kevin Gaab <kgaab@gamerparty.eu>
2021-11-15 18:17:53 +0100

    [ClientSide] Switched from mp3 to wav

---
+++ clientside/lua/autorun/resources.lua
@@ -0,0 +1,6 @@
+--endroundmusic
+resource.AddFile( "sound/endroundmusic/innocent/gasstation.wav" );
+resource.AddFile( "sound/endroundmusic/traitor/wildfire.wav" );
+
+--custom Songs Dance Gun
+resource.AddFile("sound/songs/wdtfs.wav")
