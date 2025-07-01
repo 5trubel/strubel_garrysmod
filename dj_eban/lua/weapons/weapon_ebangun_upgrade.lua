b9d3a14b98ae2886d7e20976767d384652eba122
Kevin Gaab <kontakt@kevingaab.de>
2025-07-01 22:24:12 +0200

    [DJ Eban] Started with the Pack a punch upgrade...

---
+++ dj_eban/lua/weapons/weapon_ebangun_upgrade.lua
@@ -0,0 +1,11 @@
+local UPGRADE = {}
+UPGRADE.id = "dj_eban"
+UPGRADE.name = "DJ Eban"
+UPGRADE.desc = "x2 damage, да?"
+UPGRADE.damageMult = 2
+
+function UPGRADE:Apply(SWEP)
+
+end
+
+TTTPAP:Register(UPGRADE)
