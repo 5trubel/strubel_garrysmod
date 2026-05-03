e9185885f10cc6cd066953a7898e39d73d7417cd
Kevin Gaab <kontakt@kevingaab.de>
2026-05-03 22:33:34 +0200

    [HDTF] Added axe and axe

---
+++ huntdownthefreeman-ttt/gamemodes/terrortown/entities/weapons/weapon_hdtf_axe.lua
@@ -0,0 +1,23 @@
+AddCSLuaFile()
+
+if file.Exists("lua/weapons/hdtf_axe/shared.lua", "GAME") then
+	include("weapons/hdtf_axe/shared.lua")
+	AddCSLuaFile("weapons/hdtf_axe/shared.lua")
+end
+
+if CLIENT then
+	SWEP.PrintName = "Axe"
+	SWEP.Slot = 5
+	SWEP.Icon = "sprites/hdtf/axe"
+	SWEP.IconLetter = "a"
+end
+
+
+SWEP.Kind = WEAPON_EQUIP 
+SWEP.AutoSpawnable = true
+SWEP.AmmoEnt = ""
+SWEP.InLoadoutFor = {nil}
+SWEP.AllowDrop = true
+SWEP.IsSilent = true
+SWEP.NoSights = true
+SWEP.Primary.Ammo = ""
