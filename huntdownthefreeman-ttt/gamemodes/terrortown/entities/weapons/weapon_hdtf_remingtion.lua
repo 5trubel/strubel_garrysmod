b72a4fab33b43faacccf6a6417c69eedd9197e5f
Kevin Gaab <kontakt@kevingaab.de>
2026-05-05 20:40:56 +0200

    [HDTF] Buffed MP7; Added Remingtion

--- huntdownthefreeman-ttt/gamemodes/terrortown/entities/weapons/weapon_hdtf_remingtion.lua
+++ huntdownthefreeman-ttt/gamemodes/terrortown/entities/weapons/weapon_hdtf_remingtion.lua
@@ -1,33 +1,31 @@
 AddCSLuaFile()
 
-if file.Exists("lua/weapons/hdtf_smg1/shared.lua", "GAME") then
-    include("weapons/hdtf_smg1/shared.lua")
-    AddCSLuaFile("weapons/hdtf_smg1/shared.lua")
+if file.Exists("lua/weapons/hdtf_shotgun/shared.lua", "GAME") then
+    include("weapons/hdtf_shotgun/shared.lua")
+    AddCSLuaFile("weapons/hdtf_shotgun/shared.lua")
 end
 
 if CLIENT then
-    SWEP.PrintName = "MP7"
-    SWEP.Slot = 2
-    SWEP.Icon = "sprites/hdtf/smg1"
+    SWEP.PrintName = "Remington 870"
+    SWEP.Slot = 3
+    SWEP.Icon = "sprites/hdtf/r870"
     SWEP.IconLetter = "a"
 end
 
-SWEP.ViewModelFOV = 54
-
-SWEP.Primary.Ammo = "smg1"
-
+SWEP.Primary.Ammo = "Buckshot"
 SWEP.Kind = WEAPON_HEAVY
 SWEP.AutoSpawnable = true
-SWEP.AmmoEnt = "item_ammo_smg1_ttt"
+SWEP.AmmoEnt = "item_box_buckshot_ttt"
 SWEP.InLoadoutFor = {nil}
 SWEP.AllowDrop = true
 SWEP.IsSilent = false
 SWEP.NoSights = false
 
+
 hook.Add("ScalePlayerDamage", "HDTF_ScalePlayerDamage", function(ply, hitgroup, dmginfo)
 	if ply:GetActiveWeapon() == SWEP then
 		local wep = ply:GetActiveWeapon()
-		if wep:GetClass() == "weapon_hdtf_mp7" then
+		if wep:GetClass() == "weapon_hdtf_shotgun" then
 			print("Hitgroup: " .. hitgroup)
 			if hitgroup == HITGROUP_HEAD then
 				dmginfo:SetDamage(dmginfo:GetDamage() * 20)
